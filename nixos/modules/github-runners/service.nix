ad84624a9f51f983c92c1bd4db7ae8f46d4df353
Tino Breddin <t@tolbrino.com>
2024-03-30 20:15:54 +0100

    gh-runner: Add limited resources option

--- nixos/modules/github-runners/service.nix
+++ nixos/modules/github-runners/service.nix
@@ -149,7 +149,7 @@ in
             }
             clean_state() {
               find "$STATE_DIRECTORY/" -mindepth 1 -delete
-              copy_tokens 
+              copy_tokens
             }
             unregister_previous_runner
             clean_state
